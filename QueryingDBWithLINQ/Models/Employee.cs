b9b893bd52355b7d1937ec403a71cbdcffbc6212
AT <AT@DESKTOP-AKM319L>
2024-09-09 21:42:49 +0500

    Add project files.

---
+++ QueryingDBWithLINQ/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryingDBWithLINQ.Models
+{
+    internal class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
---
+++ QueryingDBWithLINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using QueryingDBWithLINQ.Models;
+
+Employee[] employeeArray = [
+    new Employee(){EmployeeID=1, EmployeeName="Peter", Salary=12000},
+    new Employee(){EmployeeID=2, EmployeeName="Linta", Salary=32000},
+    new Employee(){EmployeeID=3, EmployeeName="John", Salary=12300},
+    new Employee(){EmployeeID=4, EmployeeName="Bob", Salary=7000},
+    new Employee(){EmployeeID=5, EmployeeName="Ali", Salary=3000},
+    new Employee(){EmployeeID=6, EmployeeName="Laiba", Salary=12765},
+    new Employee(){EmployeeID=7, EmployeeName="Fatima", Salary=3400}
+    ];
+
+//Use LINQ to find below 20000 salary employee
+Console.WriteLine("==========EMP by Salary under 20K============");
+Employee[] employees = employeeArray.Where(s => s.Salary < 20000).ToArray();
+foreach (var emp in employees)
+{
+    Console.WriteLine(emp.EmployeeName);
+}
+
+//Use LINQ to find the emp with name Linta 
+Console.WriteLine("==========EMP by NAME============");
+Employee[] employeesByName = employeeArray.Where(s => s.EmployeeName.Equals("Linta")).ToArray();
+foreach (var emp in employeesByName)
+{
+    Console.WriteLine(emp.EmployeeName);
+}
+
+//Use LINQ to find the emp by ID 
+Console.WriteLine("==========EMP by ID============");
+Employee[] employeesByID = employeeArray.Where(s => s.EmployeeID==4).ToArray();
+foreach (var emp in employeesByID)
+{
+    Console.WriteLine(emp.EmployeeName);
+}
